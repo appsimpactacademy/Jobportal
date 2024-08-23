6a37e1ce50ddc58b417f9f98319c8b5e1431c076
Ravi Singh <training.appsimpactacademy@gmail.com>
2024-08-23 20:45:00 +0530

    added filtering for active jobs on public view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,13 +1,13 @@
 class JobsController < ApplicationController
   def index
     if params[:type].present?
-      @jobs = Job.includes(:company).send("#{params[:type]}_jobs")
+      @jobs = Job.includes(:company).send("#{params[:type]}_jobs").active_jobs
     else
       @jobs = Job.includes(:company).find_each
     end
   end
 
   def show
-    @job = Job.find_by(uuid: params[:id])
+    @job = Job.active_jobs.find_by(uuid: params[:id])
   end
 end
