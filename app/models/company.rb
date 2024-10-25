b2aded9e181d5774aa7e88f1a9616a85625757d0
Ravi Singh <training.appsimpactacademy@gmail.com>
2024-10-25 19:36:02 +0530

    bug fixes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ApplicationRecord
   has_many :jobs
+  validates :name, presence: true, uniqueness: true
+  validates :address, :contact, presence: true, uniqueness: true
 
   def self.ransackable_attributes(auth_object = nil)
     ["address", "contact", "created_at", "id", "name", "updated_at"]
