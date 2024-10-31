1bd69330506fbd518a927c9e471918b00de8d1bf
Ravi Singh <training.appsimpactacademy@gmail.com>
2024-10-31 15:02:39 +0530

    override registration controller create action

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,36 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    if params[:company_name].present?
+      company = Company.find_or_initialize_by(name: params[:company_name])
+      unless company.persisted?
+        company.save(validate: false)
+      end
+
+      build_resource(sign_up_params)
+      resource.company_id = company.id
+      resource.save
+      yield resource if block_given?
+      if resource.persisted?
+        if resource.active_for_authentication?
+          set_flash_message! :notice, :signed_up
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
+      end
+    else
+      super
+    end
+  end
 
   # GET /resource/edit
   # def edit
@@ -42,7 +69,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :first_name, :last_name, :contact_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :first_name, :last_name, :contact_number, :role])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
